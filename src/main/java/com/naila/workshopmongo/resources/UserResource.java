6a7c3a19f7dd77c5d4bb916a3119a5ddce281a4b
nailaoli <nailaoli@gmail.com>
2020-10-04 21:31:58 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/naila/workshopmongo/resources/UserResource.java
+++ src/main/java/com/naila/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.naila.workshopmongo.domain.Post;
 import com.naila.workshopmongo.domain.User;
 import com.naila.workshopmongo.dto.UserDTO;
 import com.naila.workshopmongo.services.UserService;
@@ -38,6 +39,7 @@ public class UserResource {
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
+	
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO userDto) {
 		User user = service.fromDTO(userDto);
@@ -61,6 +63,14 @@ public class UserResource {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+//		O atributo lista de posts no User é lazy, porém como chamamos o .getPosts() no endpoint, essa lista será
+//		carregada
 	}
 	
 }
