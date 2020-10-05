9607f8ce169a6c98d2519f82887941d259471270
nailaoli <nailaoli@gmail.com>
2020-10-04 21:40:23 -0300

    Obtendo posts e posts por id

---
+++ src/main/java/com/naila/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.naila.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.naila.workshopmongo.domain.Post;
+import com.naila.workshopmongo.repository.PostRepository;
+import com.naila.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
